62e2636bdf6be7255303cd4cc2697e12563efdd4
Gian-Franco-L <GianXero@hotmail.com>
2023-03-08 18:48:21 -0300

    trying different mongodb links, users, network access

--- mongo.js
+++ mongo.js
@@ -1,26 +1,18 @@
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
-// const connectionString = process.env.MONGO_DB_URI
+const connectionString = process.env.MONGO_DB_URI
 
-// mongoose.connect(connectionString)
-//   .then(() =>{
-//     console.log('Database connected')
-//   }).catch(err =>{
-//     console.error(err)
-//   })
+mongoose.connect(connectionString)
+  .then(() =>{
+    console.log('Database connected')
+  }).catch(err =>{
+    console.error(err)
+  })
 
-// process.on('uncaughtException', err =>{
-//   console.error(err)
-//   mongoose.disconnect()
-// })
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.MONGO_DB_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  client.close();
-});
-
+process.on('uncaughtException', err =>{
+  console.error(err)
+  mongoose.disconnect()
+})
 
 // const mysql = require('mysql')
 
